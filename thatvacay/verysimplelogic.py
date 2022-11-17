8ad8aad1ee5bb08557985cf3e02854f378fad5c3
liya-zhu <julia.liya.zhu@gmail.com>
2022-11-17 17:27:11 -0500

    it lets u add new ppl now

--- thatvacay/verysimplelogic.py
+++ thatvacay/verysimplelogic.py
@@ -5,14 +5,20 @@ app = Flask(__name__)
 CORS(app)
 
 transactions = [
-    {'From':"Julia", "To":"Sherry",'Amount':30}
+    #{'From':"Julia", "To":"Sherry",'Amount':30}
 ]
 
 balances = [
-    {'Name': 'Julia', 'Balance': -50},
-    {'Name': 'Sherry', 'Balance': 30},
-    {'Name': 'Addy', 'Balance': 20}
+    {'Name': 'Julia', 'Balance': 0},
+    {'Name': 'Sherry', 'Balance': 0},
+    {'Name': 'Addy', 'Balance': 0}
 ]
+
+optimized_routes = [
+    
+]
+
+
 
 @app.route('/transactions')
 def get_transactions():
@@ -27,12 +33,24 @@ def add_transaction():
     newtransaction = request.get_json()
     transactions.append(newtransaction)
     #add transfer amt to first person balance and minus from second
+    sender_exists = False
+    receiver_exists = False
+
     for i in range(len(balances)):
         if balances[i]["Name"] == newtransaction["From"]:
             balances[i]["Balance"] = balances[i]["Balance"] + newtransaction["Amount"]
+            sender_exists = True
 
         if balances[i]["Name"] == newtransaction["To"]:
             balances[i]["Balance"] = balances[i]["Balance"] - newtransaction["Amount"]
+            receiver_exists = True
+
+    if sender_exists == False:
+        balances.append({'Name': newtransaction["From"], 'Balance': newtransaction["Amount"]})
+
+    if receiver_exists == False:
+        balances.append({'Name': newtransaction["To"], 'Balance': -newtransaction["Amount"]})
+
     return '', 204
     
 app.run(debug = True, port = 8080)
