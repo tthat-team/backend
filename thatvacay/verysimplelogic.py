584432a5c660ef8b839da214724c4a027548cba1
liya-zhu <julia.liya.zhu@gmail.com>
2022-11-23 01:19:20 -0500

    fixed post transactions

--- thatvacay/verysimplelogic.py
+++ thatvacay/verysimplelogic.py
@@ -93,18 +93,18 @@ def update_balances_transfer(newtransfer): #add transfer amt to first person bal
     receiver_exists = False
     for i in range(len(balances)):
         if balances[i]["Name"] == newtransfer["From"]:
-            balances[i]["Balance"] = balances[i]["Balance"] + newtransfer["Amount"]
+            balances[i]["Balance"] = balances[i]["Balance"] + float(newtransfer["Amount"])
             sender_exists = True
 
         if balances[i]["Name"] == newtransfer["To"]:
-            balances[i]["Balance"] = balances[i]["Balance"] - newtransfer["Amount"]
+            balances[i]["Balance"] = balances[i]["Balance"] - float(newtransfer["Amount"])
             receiver_exists = True
 
     if sender_exists == False: #theres def a better way to check if a value is in a dict
-        balances.append({'Name': newtransfer["From"], 'Balance': newtransfer["Amount"]})
+        balances.append({'Name': newtransfer["From"], 'Balance': float(newtransfer["Amount"])})
 
     if receiver_exists == False:
-        balances.append({'Name': newtransfer["To"], 'Balance': -newtransfer["Amount"]})
+        balances.append({'Name': newtransfer["To"], 'Balance': - float(newtransfer["Amount"])})
 
 
 # optimize_route() adds transfers with the format {'From': NAME1, "To": NAME2,'Amount': Num} to optimized_routes
