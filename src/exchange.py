f59ab2e658f8b00e1ce8856683b7acf84b5abb52
sherryshifeng@gmail.com <sherryshifeng@gmail.com>
2023-01-09 22:30:17 -0500

    Added working exchange rate API

---
+++ src/exchange.py
@@ -0,0 +1,19 @@
+from datetime import date
+import requests
+import json
+
+def get_exchange_rate(base_currency,currency1):
+    
+    today = date.today()
+    # modifies the url to access text from api, then stores the rate info
+    
+    url = 'https://api.exchangerate.host/timeseries?start_date='+str(today)+'&end_date='+str(today)+'&base='+base_currency+'&symbols='+currency1
+    jsonRates= requests.get(url)
+    jsonObject = json.loads(jsonRates.text)
+    rates = jsonObject["rates"]
+    
+    value = rates[str(today)]
+    ex_rate = value[currency1]
+    return ex_rate
+
+print(get_exchange_rate("USD","CAD"))
