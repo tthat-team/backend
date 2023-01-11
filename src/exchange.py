2c6e615e2b91e5abdb03d92d13e54eb88ea77455
sherryshifeng@gmail.com <sherryshifeng@gmail.com>
2023-01-10 22:26:07 -0500

    optimized optimize_routes

--- src/exchange.py
+++ src/exchange.py
@@ -2,18 +2,21 @@ from datetime import date
 import requests
 import json
 
-def get_exchange_rate(base_currency,currency1):
+def get_exchange_rate(base_currency, ex_currency):
     
     today = date.today()
     # modifies the url to access text from api, then stores the rate info
     
-    url = 'https://api.exchangerate.host/timeseries?start_date='+str(today)+'&end_date='+str(today)+'&base='+base_currency+'&symbols='+currency1
+    url = 'https://api.exchangerate.host/timeseries?start_date='+str(today)+'&end_date='+str(today)+'&base='+base_currency+'&symbols='+ex_currency
     jsonRates= requests.get(url)
     jsonObject = json.loads(jsonRates.text)
     rates = jsonObject["rates"]
     
     value = rates[str(today)]
-    ex_rate = value[currency1]
+    ex_rate = value[ex_currency]
     return ex_rate
 
 print(get_exchange_rate("USD","CAD"))
+
+    
+
---
+++ src/optimize_routes.py
@@ -0,0 +1,33 @@
+from operator import itemgetter
+
+# optimize_route() adds transfers with the format {'From': NAME1, "To": NAME2,'Amount': Num} to optimized_routes
+def optimize_route(): 
+    optimized_routes = []
+    copy_balances = [{'Name': x["Name"], 'Balance': x["Balance"]} for x in balances]
+    sorted_balances = sorted(copy_balances, key=itemgetter("Balance"), reverse=True)
+    creditors = []
+    debtors = []
+
+    for i in range(len(sorted_balances)):
+        if (sorted_balances[i]["Balance"])>0:
+            creditors.append(sorted_balances[i])
+        elif (sorted_balances[i]["Balance"])<0:
+            debtors.append(sorted_balances[i])
+    
+    debtors = debtors[::-1]
+
+    i=0
+    j=0
+    while i < len(creditors) and j < len(debtors):
+        amount = min(creditors[i]["Balance"], abs(debtors[j]["Balance"]))
+        new_route = {'From':debtors[j]["Name"], "To":creditors[i]["Name"],'Amount': amount}
+        optimized_routes.append(new_route)
+
+        debtors[j]["Balance"] += amount
+        creditors[i]["Balance"] -= amount
+
+        if debtors[j]["Balance"]  == 0: j+=1
+        if creditors[i]["Balance"] == 0: i+=1
+
+    return optimized_routes
+
